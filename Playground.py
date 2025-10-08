76193ed58877a4f88aca9c7cb348ba894749956c
Rajithan Sittampalam <rajithan.sittampalam@students.fhnw.ch>
2025-10-08 06:58:39 +0000

    save

---
+++ Playground.py
@@ -0,0 +1,47 @@
+msg = ("hello world")
+
+print(msg)
+
+
+# This is a comment
+print("this is a comment") #This is also a comment 
+
+""" This is a multi-line comment
+    you can write as much as you want
+    and it will be ignored by the interpreter
+    as long as you keep it within the triple quotes"""
+
+
+input("Tell me your favorite number:")
+
+if x > 2: print("your number is greater than two")
+else: print("your number is not greater than two")
+
+
+
+name = "raji"
+
+if name startswith("Ra"):
+    print("Hello Raji")
+else: print("Hello someone else")
+
+
+name = ""
+if name:
+    print("sring is here")
+else: print("string is not here")
+
+# Wird das hier ausgeführt? Erkennt pyhton das es ein String ist?
+## Ja, Python erkennt das als String, auch wenn er leer ist. In Python wird ein leerer String als False bewertet, während ein nicht-leerer String als True bewertet wird. Daher wird in diesem Fall die else-Anweisung ausgeführt und "string is not here" wird ausgegeben.
+
+
+
+name = input ("What is your name?")
+
+if name:
+    print(f"the Name is given: (Raji)")
+
+else: print("The name was not given" \")
+            
+#f = Format wobei die Klammern als Variable erkannt wird also 
+
